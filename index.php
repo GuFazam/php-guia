6f91292d746b7387e0363a3f5b2e505cd0ee7667
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-05-24 20:24:41 -0300

    .

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// Sintaxe e Variáveis
+# comentário
+/*comenrário
+ de várias linhas*/
+
+ echo "Olá";
+ echo "<h1>PHP - Estudando</h1>";
+ print("<h3>Aula 02</h3>");
+ print_r("Vamos estudar!!!"); // serve para debugar, mostrar a linha de código comumente usado paar identificar erros
+
+ //pode se utilizar '' ou ""
+ echo '<h3>Mensagem</h3>'; //todos os comandos terminam com ;
+ echo 9.99; //usa-se ponto ao invés de virgula para numeros quebrados
+?>
