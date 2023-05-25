736bafe9a8891d97e0f96940c1e688d1054c3368
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-05-24 22:20:27 -0300

    .

---
+++ aula02.php
@@ -0,0 +1,14 @@
+<?php
+//operadores relacionais
+echo (20 > 30) . " FALSE <br>"; //maior >
+echo (20 < 30 ) . " TRUE <br>"; //menor <
+echo (10 == 10) . " TRUE <br>"; //igual ==
+echo (10 != 10) . " FALSE <br>"; //diferente !=
+echo (10 >= 10) . " TRUE <br>"; //maior ou igual >=
+echo (10 <= 10) . " TRUE <br>"; //maior ou igual <=
+
+//operadores lógicos
+echo (10 == 10 && 10 > 5) . " TRUE <br>"; //operador and(E) &&
+echo (10 == 10 || 10 > 5) . " TRUE <br>"; //operador or(OU) ||
+echo (!false) . " TRUE <br>";//operador não !, ele sempre inverte
+?>
